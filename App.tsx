ea061939b25820961218087f7ef0a394f0f4f57b
annekarolinefc <58259238+annekarolinefc@users.noreply.github.com>
2022-11-07 10:55:16 -0300

    1 file modified, 2 added, 1 deleted

--- App.tsx
+++ App.tsx
@@ -4,8 +4,38 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Sistema de Gerenciamento de Aeroportos</h1>
+      <button className="button-add">Adicionar</button>
+
+      <section>
+        <table>
+          <thead>
+            <th>ID</th>
+            <th>Nome do Aeroporto</th>
+            <th>Nome Completo</th>
+            <th>IATA</th>
+            <th>Cidade</th>
+            <th>Codigo do País</th>
+            <th>Latitude</th>
+            <th>Longitude</th>
+            <th>Altitude</th>
+          </thead>
+          <tbody>
+            <tr *ngFor="let repositorio of repositorios;>
+              <td>{{ repositorio.id_aeroporto }}</td>
+              <td>{{ repositorio.nome_aeroporto }}</td>
+              <td>{{ repositorio.codigo_iata }}</td>
+              <td>{{ repositorio.cidade }}</td>
+              <td>{{ repositorio.codigo_pais_iso }}</td>
+              <td>{{ repositorio.latitude }}</td>
+              <td>{{ repositorio.longitude }}</td>
+              <td>{{ repositorio.altitude }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+
     </div>
+   
   );
 }
---
+++ controllers/repositorio.js
@@ -0,0 +1,10 @@
+export interface Repositorio {
+  id: number,
+  nome_aeroporto: string,
+  codigo_iata: string,
+  cidade: string,
+  codigo_pais_iso: string,
+  latitude: number,
+  longitude: number,
+  altitude: number
+}
